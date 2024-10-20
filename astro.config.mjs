c4b3640856a7d02c3e4e573ba8931a627c780f09
Truong Ngoc Chau <114427250+billtruong003@users.noreply.github.com>
2024-10-21 05:58:46 +0700

    * Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ const oklchToHex = str => {
 // https://astro.build/config
 export default defineConfig({
   site: 'https://billthedevlab.tech/',
-  base: '/src/',
+  // base: '/src/',
   trailingSlash: 'always',
   integrations: [
     tailwind(),
